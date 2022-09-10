ac78bf893e95ab7ec750db8494fa09d15f30f485
ILYAZGO <mig_star@mail.ru>
2022-09-10 13:03:09 +0300

    add step2

--- tests/params.py
+++ tests/params.py
@@ -15,7 +15,3 @@ path = os.getcwd() + filename
 age = str(random.randint(10,99))
 department = fake.color_name()
 add_letter = 'a'
-#first_name2 = fake.first_name()
-#last_name2 = fake.last_name()
-#email2 = fake.ascii_company_email()
-#ten_digits2 = str(random.randint(1111111111,9999999999))
